c9f01f40fa7855b33f1789a181bf85fa0277d51d
stlifey <stlifey@gmail.com>
2012-01-19 03:17:45 +0800

    Fix leds class (AF,RGB,KPAD)

--- arch/arm/mach-omap2/board-mapphone-cpcap-client.c
+++ arch/arm/mach-omap2/board-mapphone-cpcap-client.c
@@ -200,7 +200,7 @@ static int __init is_ld_cpcap_kpad_on(void)
 {
 	u8 device_available;
 
-     device_available = 1;
+	device_available = 0;
 	return device_available;
 }
 
@@ -216,7 +216,7 @@ static int __init cpcap_button_init(void)
 {
 	u8 device_available;
 
-    device_available = 0;
+	device_available = 0;
 	return device_available;
 }
 
@@ -228,10 +228,10 @@ static int __init ld_cpcap_disp_button_init(void)
 	ret = -ENODEV;
 
         device_available = 1;
-        pbtn_data->duty_cycle = 0x2A0;
+        pbtn_data->duty_cycle = 0xB8;
 	pbtn_data->cpcap_mask = 0x3FF;
-	pbtn_data->led_current =  0xA;
-	pbtn_data->reg = CPCAP_REG_KLC;
+	pbtn_data->led_current =  0x0;
+	pbtn_data->reg = CPCAP_REG_BLEDC;
 
 	ret = 1;
 	return ret;
