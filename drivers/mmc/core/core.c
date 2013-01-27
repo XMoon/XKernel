5cabe0ce9e4df6668d7536122cca1758e519601a
Tony Liu <fentensoft@163.com>
2013-01-27 22:27:52 +0800

    MMC delayed work fix

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -55,7 +55,7 @@ module_param(use_spi_crc, bool, 0);
 static int mmc_schedule_delayed_work(struct delayed_work *work,
 				     unsigned long delay)
 {
-	wake_lock(&mmc_delayed_work_wake_lock);
+	//wake_lock(&mmc_delayed_work_wake_lock);
 	return queue_delayed_work(workqueue, work, delay);
 }
 
@@ -532,12 +532,10 @@ void mmc_host_deeper_disable(struct work_struct *work)
 
 	/* If the host is claimed then we do not want to disable it anymore */
 	if (!mmc_try_claim_host(host))
-		goto out;
+		return;
 	mmc_host_do_disable(host, 1);
 	mmc_do_release_host(host);
 
-out:
-	wake_unlock(&mmc_delayed_work_wake_lock);
 }
 
 /**
@@ -1058,6 +1056,8 @@ void mmc_detect_change(struct mmc_host *host, unsigned long delay)
 	spin_unlock_irqrestore(&host->lock, flags);
 #endif
 
+	wake_lock(&mmc_delayed_work_wake_lock);
+
 	mmc_schedule_delayed_work(&host->detect, delay);
 }
 
@@ -1176,7 +1176,8 @@ void mmc_stop_host(struct mmc_host *host)
 
 	if (host->caps & MMC_CAP_DISABLE)
 		cancel_delayed_work(&host->disable);
-	cancel_delayed_work(&host->detect);
+	if (cancel_delayed_work_sync(&host->detect))
+		wake_unlock(&mmc_delayed_work_wake_lock);
 	mmc_flush_scheduled_work();
 
 	/* clear pm flags now and let card drivers set them as needed */
@@ -1298,7 +1299,8 @@ int mmc_suspend_host(struct mmc_host *host, pm_message_t state)
 
 	if (host->caps & MMC_CAP_DISABLE)
 		cancel_delayed_work(&host->disable);
-	cancel_delayed_work(&host->detect);
+	if (cancel_delayed_work_sync(&host->detect))
+		wake_unlock(&mmc_delayed_work_wake_lock);
 	mmc_flush_scheduled_work();
 
 	mmc_bus_get(host);
